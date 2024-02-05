59639bd327a6d49d0f5df43eecc18c8208134b9c
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-05 00:57:31 +0000

    base format set

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import App from "./component/App.jsx";
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<App />, document.querySelector("#app"));
