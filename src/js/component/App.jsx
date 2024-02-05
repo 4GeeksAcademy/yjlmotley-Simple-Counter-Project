e752111d7be9e260bec78256f3eb1b491f0a4859
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-05 02:59:10 +0000

    counter set

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SimpleCounter from "./SimpleCounter.jsx";
+import { useState } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(counter => counter + 1)
+        }, 1000)
+    console.log(counter)
+    }, []) 
     return (
         <>
             <SimpleCounter />
