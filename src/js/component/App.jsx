6120a661d47ac99024fced1437afd29611a16767
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-05 04:35:53 +0000

    counter is all set up

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import SimpleCounter from "./SimpleCounter.jsx";
 import { useState } from "react";
+import calculateSeconds from "../../lib/time.js";
+
 
 function App() {
     const [counter, setCounter] = useState(0);
@@ -9,13 +11,23 @@ function App() {
         const interval = setInterval(() => {
             setCounter(counter => counter + 1)
         }, 1000)
-    console.log(counter)
-    }, []) 
+
+    return () => clearInterval(interval)
+    }, [counter]) 
+
     return (
         <>
-            <SimpleCounter />
+            <SimpleCounter
+                hundredThousandsDigit = {calculateSeconds(counter, 100000)}
+                tenThousandsDigit = {calculateSeconds(counter, 10000)}
+                thousandsDigit = {calculateSeconds(counter, 1000)}
+                hundredsDigit = {calculateSeconds(counter, 100)}
+                tensDigit = {calculateSeconds(counter, 10)}
+                onesDigit = {calculateSeconds(counter, 1)}
+            />
         </>
     )
 }
+
 
 export default App
